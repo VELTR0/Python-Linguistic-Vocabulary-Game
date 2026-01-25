15a25c03b81bc7873f73e56b751e6123fab7177b
quang.le-hong <quang.le-hong@uni-konstanz.de>
2026-01-25 16:09:43 +0100

    black theme in 8bit

--- Game.py
+++ Game.py
@@ -20,20 +20,15 @@ pygame.display.set_caption("Learning Urdu")
 
 # Theme
 font = pygame_menu.font.FONT_8BIT
-mytheme = pygame_menu.Theme(
-                background_color=(0, 0, 0, 0), # transparent background
-                title_background_color=(4, 47, 126),
-                title_font_shadow=True,
-                widget_padding=25,
-                widget_font = font
-                
-                )
+mytheme = pygame_menu.themes.THEME_DARK
+mytheme.widget_font = font
+
 
 # Menu Setup
-menu = pygame_menu.Menu('Welcome', 400, 300, theme=mytheme)
+menu = pygame_menu.Menu('Welcome', 800, 500, theme=mytheme)
 
-menu.add.text_input('Name :', default='John Doe')
-menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+menu.add.text_input('PLEASE ENTER NAME :', default='')
+menu.add.selector('Gamemode :', [('<Vocabulary>', 1), ('←Grammer→', 2), ('←Test→', 3)], onchange=set_difficulty)
 menu.add.button('Play', start_the_game)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 
