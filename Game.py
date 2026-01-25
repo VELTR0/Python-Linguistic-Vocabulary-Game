d069218377170cb02d4f9743da7dac3e6ed1978f
quang.le-hong <quang.le-hong@uni-konstanz.de>
2026-01-25 15:41:16 +0100

    theme

--- Game.py
+++ Game.py
@@ -18,8 +18,19 @@ def start_the_game():
 screen = pygame.display.set_mode((1100,500),0,32)
 pygame.display.set_caption("Learning Urdu")
 
+# Theme
+font = pygame_menu.font.FONT_8BIT
+mytheme = pygame_menu.Theme(
+                background_color=(0, 0, 0, 0), # transparent background
+                title_background_color=(4, 47, 126),
+                title_font_shadow=True,
+                widget_padding=25,
+                widget_font = font
+                
+                )
+
 # Menu Setup
-menu = pygame_menu.Menu('Welcome', 400, 300, theme=pygame_menu.themes.THEME_BLUE)
+menu = pygame_menu.Menu('Welcome', 400, 300, theme=mytheme)
 
 menu.add.text_input('Name :', default='John Doe')
 menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
