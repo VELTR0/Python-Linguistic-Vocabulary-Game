cb9bac3b870b6eb1e9f76b5903bc1436efb48000
quang.le-hong <quang.le-hong@uni-konstanz.de>
2026-01-26 01:17:10 +0100

    sprites for haze

--- PraiseOrHaze.py
+++ PraiseOrHaze.py
@@ -1,16 +1,23 @@
 import pygame
+import Vocabulary
 
 pygame.init()
 
-
+# Sprites
+selector =            pygame.image.load(r"Sprites\PraiseOrHaze\Selector.png")
+correct =         pygame.image.load(r"Sprites\PraiseOrHaze\Correcto.png")
+wrong =           pygame.image.load(r"Sprites\PraiseOrHaze\Wrong.png")
 
 def startGame(screen):
     game_running = True
-    screen.fill((0, 128, 0))
-    print("1") 
+    screen.fill((0, 0, 0))
     while game_running:
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
                 game_running = False
+
+        font_size = 
+
+            
         pygame.display.update()
